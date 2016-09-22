bf8b8d35869da27ea029b9c59fe01eff401fbc6f
somename <someemail>
2016-09-22 15:00:40 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -15,6 +15,16 @@ public class TestBowling {
 	}
 	
 	@Test
+	public void test_GetScoreOfFrame_Returns_9() {
+		//Arrange
+		Frame frame = new Frame(10, 0);
+		//Act
+		boolean result = frame.isStrike();
+		//Assert
+		assertEquals(true, result);
+	}
+	
+	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
