67a9b27a5dc74951ba9c76605d9dfcd0191e2db7
somename <someemail>
2016-09-22 14:58:27 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -7,9 +7,11 @@ public class TestBowling {
 	@Test
 	public void test_GetScoreOfFrame_Returns_9() {
 		//Arrange
-		
+		Frame frame = new Frame(5, 4);
 		//Act
+		int result = frame.score();
 		//Assert
+		assertEquals(9, result);
 	}
 	
 	@Test
